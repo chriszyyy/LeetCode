69bdf96c8b31e1cc17dc5a4a352b9cd6125ca72a
chris <chriszyyy@gmail.com>
2020-08-12 16:53:23 +0200

    pb-231: fix edge cases

--- 231/power_of_two/s2.bitwise_ops.js
+++ 231/power_of_two/s2.bitwise_ops.js
@@ -6,7 +6,7 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if (n === 0) return false;
+    if (n <= 0) return false;
     const x = n;
-    return (x & (-x)) === x; // 带符号整数范围有问题 -2147483648
+    return (x & (-x)) === x;
 };
--- 231/power_of_two/s3.bitwise_ops_2.js
+++ 231/power_of_two/s3.bitwise_ops_2.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if (n === 0) return false;
+    if (n <= 0) return false;
     const x = n;
-    return (x & (x - 1)) === 0; // 带符号整数范围有问题 -2147483648
+    return (x & (x - 1)) === 0;
 };
