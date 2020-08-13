aceed9f37dbd44b4a3489942f6aaa5eba833e22f
chris <chriszyyy@gmail.com>
2020-08-14 01:50:59 +0200

    pb-98: validate binary search tree

---
+++ 98_validate_binary_search_tree/s1.recursive.D&C.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+
+    function helper(node, lower=-Infinity, upper=Infinity) {
+        if (!node) return true;
+
+        const val = node.val;
+
+        if (val <= lower || val >= upper) return false;
+
+        if (!helper(node.left, lower, val)) return false;
+
+        if (!helper(node.right, val, upper)) return false;
+
+        return true;
+    }
+
+    return helper(root);
+};
